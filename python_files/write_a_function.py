4dea59650aa22a439ebfa55a94b9bc12161e6646
Sreekaanth Ganesan <sreekaanthg91@gmail.com>
2022-01-29 19:17:56 -0500

    Q4

---
+++ python_files/write_a_function.py
@@ -0,0 +1,18 @@
+def is_leap(year):
+    leap = False
+    
+    # Write your logic here
+    if year %4 ==0:
+        if year %100 == 0 and year %400 !=0:
+            leap= False
+        elif year %400 ==0 and year % 100==0:
+            leap=True
+        else:
+            leap=True
+        # leap = True
+    else:
+        leap = False
+    
+    return leap
+
+year = int(input())

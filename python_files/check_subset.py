df3bb9d4b5fe0e4e3bcc5579a8401b6276ff5f7b
Sreekaanth Ganesan <sreekaanthg91@gmail.com>
2022-02-20 16:36:27 -0500

    check Subset

---
+++ python_files/check_subset.py
@@ -0,0 +1,5 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+for _ in range(int(input())):
+    x, a, z, b = input(), set(input().split()), input(), set(input().split())
+    print(a.issubset(b))

1489609dce22d0df95b5a40e5de4ae2f910d1c58
Sreekaanth Ganesan <sreekaanthg91@gmail.com>
2022-02-27 13:12:12 -0500

    string formatting

---
+++ python_files/string_formatting.py
@@ -0,0 +1,16 @@
+def print_formatted(N):
+    # your code goes here
+    
+    l = len(bin(N)) - 2
+
+    for i in range(1, N + 1):
+        f = ""
+        for c in "doXb":
+            if f:
+                f += " "
+            f += "{:>" + str(l) + c + "}"
+        print(f.format(i, i, i, i))
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
